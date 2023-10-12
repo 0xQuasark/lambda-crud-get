fa5dacf710cc254051b93172f46a5bfed3b1f9df
Quasark <q@quasark.xyz>
2023-10-12 10:42:46 -0700

    GET id tweak

--- index.js
+++ index.js
@@ -14,14 +14,18 @@ let results = null;
 let personModel = dynamoose.model('cf-lambda-crud', peopleSchema); // first argument is the name of the table
 
 exports.handler = async (event) => {
-  console.log('crudGET 1.2');
+  console.log('crudGET 1.3');
   console.log('HERE IS THE EVENT OBJECT', event)
   // console.log('Here are the path parameters: ', event.pathParameters)
 
-  try {
-    results = await personModel.scan().exec();
-  } catch (error) {
-    console.log('ERROR: ', error)
+  if (event.pathParameters.id) {
+    results = await personModel.query('id').eq(event.pathParameters.id).exec();
+  } else {
+    try {
+      results = await personModel.scan().exec();
+    } catch (error) {
+      console.log('ERROR: ', error)
+    }
   }
 
   // TODO implement
