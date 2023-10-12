5aba63bfff26b1c2935fce2d1a3a99f0ac152b12
Quasark <q@quasark.xyz>
2023-10-11 17:41:11 -0700

    18.10

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const dynamoose = require('dynamoose');
+ 
+// define our schema
+const peopleSchema = new dynamoose.Schema({
+  'id': Number,
+  'name': String,
+  'email': String,
+});
+
+let results = null;
+// create our Model
+let personModel = dynamoose.model('cf-lambda-crud', peopleSchema); // first argument is the name of the table
+
+exports.handler = async (event) => {
+  console.log('crudGET 1.1');
+  console.log('HERE IS THE EVENT OBJECT', event)
+  // console.log('Here are the path parameters: ', event.pathParameters)
+
+  try {
+    results = await personModel.scan().exec();
+  } catch (error) {
+    console.log('ERROR: ', error)
+  }
+
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(results),
+  };
+  return response;
+};
